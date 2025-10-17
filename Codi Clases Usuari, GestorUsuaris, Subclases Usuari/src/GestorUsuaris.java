8345146542aba88dfebe9ef62ac8e5d7e6caf67d
arnau.hernandez.navarro <arnau.hernandez.navarro@estudiantat.upc.edu>
2025-10-17 18:03:41 +0000

    Upload New File

---
+++ Codi Clases Usuari, GestorUsuaris, Subclases Usuari/src/GestorUsuaris.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class GestorUsuaris {
+    // List to store users
+    private List<Usuari> usuaris;
+
+    // Constructor
+    public GestorUsuaris() {
+        usuaris = new ArrayList<>();
+    }
+
+    // Method to add a user
+    public void afegirUsuari(Usuari usuari) {
+        this.usuaris.add(usuari);
+    }
+
+    // Method to list all users
+    public void llistarUsuaris() {
+        for (Usuari u : usuaris) {
+            System.out.println(
+                    "ID: " + u.getId()
+                            + ", Usuari: " + u.getUsuari()
+                            + ", Email: " + u.getEmail()
+                            + ", Especialitat: " + ((u instanceof PerfilEnquestat) ? ((PerfilEnquestat) u).getEspecialitat() : "N/A")
+            );
+        }
+    }
+}
