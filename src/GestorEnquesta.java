94b9a44ee4a0b65796963b5128d0b740b8504f01
mireia.brufau <mireia.brufau@estudiantat.upc.edu>
2025-10-23 22:49:52 +0200

    Actualitzat GestorEnquesta.java

--- src/GestorEnquesta.java
+++ src/GestorEnquesta.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GestorEnquesta {
+    private List<Enquesta> enquestes;
+
+    public GestorEnquesta() {
+        enquestes = new ArrayList<>();
+    }
+
+    public void afegirEnquesta(Enquesta enquesta) {
+        enquestes.add(enquesta);
+    }
+
+    // public List<Enquesta> getEnquestes() { return enquestes;} fem això també?
+
+    public void llistarEnquestes() {
+        for (Enquesta enq : enquestes) {
+            System.out.println(
+                    "Enquesta ID: " + enq.getId() +
+                            ", Títol: " + enq.getTitol() +
+                            ", Descripció: " + enq.getDescripcio() +
+                            ", Creador: " + enq.getCreador().getUsuari()
+            );
+        }
+    }
 }
