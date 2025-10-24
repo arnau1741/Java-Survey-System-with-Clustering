7e57986b2d0641e289a82ee6760f5c47bae2aaf5
mireia.brufau <mireia.brufau@estudiantat.upc.edu>
2025-10-24 13:24:44 +0200

    subir

--- src/Enquesta.java
+++ src/Enquesta.java
@@ -4,20 +4,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class Enquesta {
-    private final int id; // és final perquè no canvia mai
+    private Integer id;
     private String titol;
     private String descripcio;
     private Usuari creador;
 
-    private int seguentPreguntaID = 0;
+    private Integer seguentPreguntaID = 0;
 
     private List<Pregunta> preguntes;
 
+    // posem les dates com atributs, creem una classe Data i es relaciona?
     // private LocalDateTime dataCreacio;
     // private LocalDateTime dataFinalitzacio;
 
     // Constructora
-    public Enquesta(int id, String titol, String descripcio, Usuari creador) {
+    public Enquesta(Integer id, String titol, String descripcio, Usuari creador) {
         this.id = id;
         this.titol = titol;
         this.descripcio = descripcio;
@@ -27,10 +28,36 @@ public class Enquesta {
     }
 
     // Getters
-    public int getId() { return id;}
+    public Integer getId() { return id;}
     public String getTitol() { return titol;}
     public String getDescripcio() { return descripcio;}
     public Usuari getCreador() { return creador;}
     // public LocalDateTime getDataCreacio() { return dataCreacio; }
     // public LocalDateTime getDataFinalitzacio() { return dataFinalitzacio; }
+
+    public void afegirPregunta(Pregunta p) {
+        p.setId(seguentPreguntaID++);
+        preguntes.add(p);
+    }
+
+    public void eliminarPregunta(int preguntaID) {
+        if (preguntaID >= 0 && preguntaID < preguntes.size()) {
+            preguntes.removeIf(p -> p.getId() == preguntaID);
+        }
+    }
+
+    public void modificarPregunta(Integer preguntaID, Pregunta novaPregunta) {
+        for (int i = 0; i < preguntes.size(); i++) {
+            Pregunta p = preguntes.get(i);
+            if (p.getId().equals(preguntaID)) {
+                novaPregunta.setId(preguntaID);
+                preguntes.set(i, novaPregunta);
+                break;
+            }
+        }
+    }
+
+    public List<Pregunta> getPreguntes() {
+        return Collections.unmodifiableList(preguntes);
+    }
 }
--- src/GestorEnquesta.java
+++ src/GestorEnquesta.java
@@ -15,39 +15,29 @@ public class GestorEnquesta {
 
     // public List<Enquesta> getEnquestes() { return enquestes;} fem això també?
 
-    public void afegirPregunta(Pregunta p) {
-        p.setID(seguentPreguntaID++);
-        preguntes.add(p);
+    public void afegirPregunta(Enquesta e, Pregunta p) {
+        e.afegirPregunta(p);
     }
 
-    public void eliminarPregunta(int preguntaID) {
-        if (preguntaID >= 0 && preguntaID < preguntes.size()) {
-            preguntes.removeIf(p -> p.getID() == preguntaID);
-        }
+    public void eliminarPregunta(Enquesta e, int preguntaID) {
+        e.eliminarPregunta(preguntaID);
     }
 
-    public void modificarPregunta(Integer preguntaID, Pregunta novaPregunta) {
-        for (int i = 0; i < preguntes.size(); i++) {
-            Pregunta p = preguntes.get(i);
-            if (p.getID() == preguntaID) {
-                novaPregunta.setID(preguntaID);
-                preguntes.set(i, novaPregunta);
-                break;
-            }
-        }
+    public void modificarPregunta(Enquesta e, Integer preguntaID, Pregunta novaPregunta) {
+        e.modificarPregunta(preguntaID, novaPregunta);
     }
 
-    public List<Pregunta> getPreguntes() {
-        return Collections.unmodifiableList(preguntes);
+    public List<Pregunta> getPreguntes(Enquesta e) {
+        return e.getPreguntes();
     }
 
     public void llistarEnquestes() {
         for (Enquesta enq : enquestes) {
             System.out.println(
                     "Enquesta ID: " + enq.getId() +
-                            ", Títol: " + enq.getTitol() +
-                            ", Descripció: " + enq.getDescripcio() +
-                            ", Creador: " + enq.getCreador().getUsuari()
+                    ", Títol: " + enq.getTitol() +
+                    ", Descripció: " + enq.getDescripcio() +
+                    ", Creador: " + enq.getCreador().getUsuari()
             );
         }
     }
