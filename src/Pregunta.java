867209d64cea3465c3c3771f5764b4486618faef
andreu.percy.caro <andreu.percy.caro@estudiantat.upc.edu>
2025-10-24 11:21:04 +0200

    Subida de Pregunta

---
+++ src/Pregunta.java
@@ -0,0 +1,42 @@
+//package prop.enquesta.resposta;
+
+public class Pregunta {
+    //Necessitem un tipus per la pregunta per aixi definir la resposta
+    //En aquest cas es fara l'us de enums pero es pot fer refactoring si cal
+
+    public enum Tipus { NUMERICA, LLIURE, UNICA, MULTIPLE, ORDENADA }
+
+    private String id;
+    private String text;
+    private Tipus tipus;
+
+    //Format P1 Pregunta_qualsevol? -- Defineix tipus de resposta
+    public Pregunta(String id, String text, Tipus tipus) {
+        this.id = id;
+        this.text = text;
+        this.tipus = tipus;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Tipus getTipus() {
+        return tipus;
+    }
+
+    @Override
+    public String toString() {
+        return "Pregunta{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", tipus=" + tipus +
+                '}';
+    }
+
+    // getters y toString()
+}
