ab6d7f1f0ca844c73b56df671dbaafa4b4aa690b
andreu.percy.caro <andreu.percy.caro@estudiantat.upc.edu>
2025-11-08 22:13:46 +0100

    Comentarios para la trata de archivos

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,19 @@ public class Main {
 
         GestorUsuaris gestor = new GestorUsuaris();
 
+        //Import
+        //Recibir un fichero (Path) -> Abrimos fichero
+        //sc = new Scanner(System.in);
+        //System.out.print("Introdueix la ruta del fitxer d'importació: ");
+        //String rutaImport = sc.nextLine();
+
+        //Llamar a funcion para tratar
+        //Obtener datos y llamar a controlador de dominio
+
+        //Export
+        //Llamo al controlador de dominio para obtener datos
+        //Abrir pipe y guardar datos en fichero
+        //Guardarlo en la ruta establecida
 
 
         sc.close();
