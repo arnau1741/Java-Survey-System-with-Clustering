2ce07441dd1cb45b6c394c70eb572d5ef6d8eb80
arnau-hern-ndez <arnau.hernandez.navarro@estudiantat.upc.edu>
2025-11-10 16:31:59 +0100

    añadido break en consultarPerfil y imagen del diagrama UML

--- src/GestorUsuaris.java
+++ src/GestorUsuaris.java
@@ -25,6 +25,8 @@ public class GestorUsuaris {
                                 + ", Email: " + u.getEmail()
                                 + ", Especialitat: " + (u.getRol())
                 );
+                //parar busqueda
+                break;
             }
         }
     }
