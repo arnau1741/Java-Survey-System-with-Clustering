87c3b34719049a720c5c0faa32540bdeeda1ccfb
mireia.brufau <mireia.brufau@estudiantat.upc.edu>
2025-11-14 17:42:39 +0100

    subir

--- src/Usuari.java
+++ src/Usuari.java
@@ -24,16 +24,66 @@ public abstract class Usuari {
     }
 
     //Get functions
+    /**
+     * Retorna l'id de l'usuari
+     * @return int idUsuari
+     */
     public int getId() { return idUsuari; }
+
+    /**
+     * Retorna el nom de l'usuari
+     * @return string nomUsuari
+     */
     public String getUsuari() { return nomUsuari; }
+
+    /**
+     * Retorna la contrasenya de l'usuari
+     * @return string contrasenya
+     */
     public String getContrasenya() { return contrasenya; }
+
+    /**
+     * Retorna l'email de l'usuari
+     * @return string email
+     */
     public String getEmail() { return email; }
+
+    /**
+     * Retorna si l'usuari esta bloquejat
+     * @return boolean blocked
+     */
     public boolean isBlocked() { return blocked; }
 
     //Set functions
+    /**
+     * Modifica el nom de l'usuari
+     * @param usuari nou nom de l'usuari
+     */
     public void setUsuari(String usuari) { this.nomUsuari = usuari; }
+
+    /**
+     * Modifica la contrasenya de l'usuari
+     * @param contrasenya nova contrasenya de l'usuari
+     */
+    public void setContrasenya(String contrasenya) { this.contrasenya = contrasenya; }
+
+    /**
+     * Modifica l'email de l'usuari
+     * @param email nou email de l'usuari
+     */
     public void setEmail(String email) { this.email = email; }
+
+    /**
+     * Modifica l'estat de bloqueig de l'usuari
+     * @param blocked nou estat de bloqueig de l'usuari
+     */
     public void setBlocked(boolean blocked) { this.blocked = blocked; }
+
+    // Metodes abstractes
+    /**
+     * Metode abstracte per eliminar una enquesta
+     * @param idEnquesta de l'enquesta
+     */
     public abstract void eliminarEnquesta(int idEnquesta);
 }
 
