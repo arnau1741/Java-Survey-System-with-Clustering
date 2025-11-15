72bd0cf612edff1aa33b8cc9f1a7641e5d2b17ee
mireia.brufau <mireia.brufau@estudiantat.upc.edu>
2025-11-15 15:55:29 +0100

    subir enqStub

---
+++ TestUnitarios/TestUnitariEnquesta_Stub.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class TestUnitariEnquesta_Stub {
+}
--- src/EnquestaStub.java
+++ src/EnquestaStub.java
@@ -1,5 +1,18 @@
 public class EnquestaStub extends Enquesta {
-    public EnquestaStub(int id) {
-        super(id, "", "", 0, new java.util.ArrayList<>());
+    private final Integer id;
+
+    public EnquestaStub(Integer id) {
+        super(id, "Stub", "Descripcio stub", 0, new java.util.ArrayList<>());
+        this.id = id;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int getNumPreguntes() {
+        return 0;
     }
 }
