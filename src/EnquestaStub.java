0c8e96a9d0850917f3c66b9f0dcdddc31787f324
arnau-hern-ndez <arnau.hernandez.navarro@estudiantat.upc.edu>
2025-11-15 20:15:47 +0100

    quitado el id de EnquestaStub, es una SUBCLASE

--- src/EnquestaStub.java
+++ src/EnquestaStub.java
@@ -1,9 +1,6 @@
 public class EnquestaStub extends Enquesta {
-    private final Integer id;
-
     public EnquestaStub(Integer id) {
         super(id, "Stub", "Descripcio stub", 0, new java.util.ArrayList<>());
-        this.id = id;
     }
 }
 
