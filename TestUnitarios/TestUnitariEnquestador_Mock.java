daa899597d2856ee4d73dd431eb22cef03d5b7f4
arnau-hern-ndez <arnau.hernandez.navarro@estudiantat.upc.edu>
2025-11-15 19:04:27 +0100

    añadido un mock y añadidas librerias para mock

--- TestUnitarios/TestUnitariEnquestador_Mock.java
+++ TestUnitarios/TestUnitariEnquestador_Mock.java
@@ -3,21 +3,31 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.fail;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matcher.*;
-import org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
-public class TestUnitariEnquestador {
+public class TestUnitariEnquestador_Mock {
 
     private PerfilEnquestador enquestador;
 
+    @Mock
+    private Enquesta enquestaMock1;
+
+    @Mock
+    private Enquesta enquestaMock2;
+
     @Before
     public void setUp() {
+        MockitoAnnotations.openMocks(this);
         enquestador = new PerfilEnquestador(
-                0,"Anna", "1234",
+                0, "Anna", "1234",
                 "anna@gmail.com");
+
+        when(enquestaMock1.getId()).thenReturn(1);
+        when(enquestaMock2.getId()).thenReturn(2);
     }
 
     @Test
@@ -31,32 +41,26 @@ public class TestUnitariEnquestador {
 
     @Test
     public void afegirEnquestaAssignadaFuncionaCorrectament() {
-        //id, titol, descripcio, idcreador, preguntes
-        Enquesta enquesta = new Enquesta(1, "Titol1", "Desc1", 2, new java.util.ArrayList<>());
-        Enquesta enquesta2 = new Enquesta(2, "Titol2", "Desc2", 3, new java.util.ArrayList<>());
-        enquestador.afegirEnquestaAssignada(enquesta);
-        enquestador.afegirEnquestaAssignada(enquesta2);
+        enquestador.afegirEnquestaAssignada(enquestaMock1);
+        enquestador.afegirEnquestaAssignada(enquestaMock2);
         assertThat(enquestador.enquestaAssignada(1), equalTo(true));
         assertThat(enquestador.enquestaAssignada(2), equalTo(true));
     }
 
     @Test
     public void eliminarEnquestaAssignadaFuncionaCorrectament() {
-        Enquesta enquesta = new Enquesta(1, "Titol1", "Desc1", 2, new java.util.ArrayList<>());
-        enquestador.afegirEnquestaAssignada(enquesta);
+        enquestador.afegirEnquestaAssignada(enquestaMock1);
         enquestador.eliminarEnquestaAssignada(1);
         assertThat(enquestador.enquestaAssignada(1), equalTo(false));
     }
 
     @Test
     public void getEnquestesAssignadesFuncionaCorrectament() {
-        Enquesta enquesta1 = new Enquesta(1, "Titol1", "Desc1", 2, new java.util.ArrayList<>());
-        Enquesta enquesta2 = new Enquesta(2, "Titol2", "Desc2", 3, new java.util.ArrayList<>());
-        enquestador.afegirEnquestaAssignada(enquesta1);
-        enquestador.afegirEnquestaAssignada(enquesta2);
+        enquestador.afegirEnquestaAssignada(enquestaMock1);
+        enquestador.afegirEnquestaAssignada(enquestaMock2);
         assertThat(enquestador.getEnquestesAssignades().size(), equalTo(2));
-        assertThat(enquestador.getEnquestesAssignades().get(0), equalTo(enquesta1));
-        assertThat(enquestador.getEnquestesAssignades().get(1), equalTo(enquesta2));
+        assertThat(enquestador.getEnquestesAssignades().get(0), equalTo(enquestaMock1));
+        assertThat(enquestador.getEnquestesAssignades().get(1), equalTo(enquestaMock2));
     }
 
     //Casos extrems
@@ -68,14 +72,8 @@ public class TestUnitariEnquestador {
 
     @Test
     public void testEliminarEnquestaInexistent() {
-        // Afegeix una enquesta
-        Enquesta enquesta = new Enquesta(1, "Titol1", "Desc1", 2, new java.util.ArrayList<>());
-        enquestador.afegirEnquestaAssignada(enquesta);
-
-        // Intenta eliminar una que no existeix
+        enquestador.afegirEnquestaAssignada(enquestaMock1);
         enquestador.eliminarEnquestaAssignada(99);
-
-        // Comprova que la llista no ha canviat i l'enquesta original segueix allà
         assertThat(enquestador.getEnquestesAssignades().size(), equalTo(1));
         assertThat(enquestador.enquestaAssignada(1), equalTo(true));
     }
