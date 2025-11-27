a64a938fc72fd1ff6ab1c80ae91084490277877c
mireia.brufau <mireia.brufau@estudiantat.upc.edu>
2025-11-27 23:22:13 +0100

    vista crearEnquesta

---
+++ src/prop/enquestes/presentacio/VistaCrearEnquesta.java
@@ -0,0 +1,10 @@
+package prop.enquestes.presentacio;
+
+import javax.swing.*;
+
+public class VistaCrearEnquesta {
+    private JTextField escriuElTítolDeTextField;
+    private JTextField escriuLaDescripcióDeTextField;
+    private JButton afegirPreguntaButton;
+    private JButton sortirButton;
+}
