6ec58b8ed141a0bd8921c59fb0abdd48d6dd9bcb
mireia.brufau <mireia.brufau@estudiantat.upc.edu>
2025-11-27 23:32:29 +0100

    vista respondreEnquesta

---
+++ src/prop/enquestes/presentacio/VistaRespondreEnquesta.java
@@ -0,0 +1,9 @@
+package prop.enquestes.presentacio;
+
+import javax.swing.*;
+
+public class VistaRespondreEnquesta {
+    private JButton sortirButton;
+    private JButton OKButton;
+    private JComboBox comboBox1;
+}
