50551757379531e66626bd5f432c39dce15522d2
mireia.brufau <mireia.brufau@estudiantat.upc.edu>
2025-11-27 23:39:46 +0100

    vista consultarRecomanacions

---
+++ src/prop/enquestes/presentacio/VistaConsultarRecomanacions.java
@@ -0,0 +1,9 @@
+package prop.enquestes.presentacio;
+
+import javax.swing.*;
+
+public class VistaConsultarRecomanacions {
+    private JComboBox comboBox1;
+    private JButton sortirButton;
+    private JButton OKButton;
+}
