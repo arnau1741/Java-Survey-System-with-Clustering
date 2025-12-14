59d9f36f766210d63fa74addd4a222c1bc37696f
andreu.percy.caro <andreu.percy.caro@estudiantat.upc.edu>
2025-12-14 12:39:30 +0100

    Es mou el boto vetar per coherencia al moderador

--- src/prop/enquestes/presentacio/VistaAdmin.java
+++ src/prop/enquestes/presentacio/VistaAdmin.java
@@ -28,7 +28,7 @@ public class VistaAdmin extends JFrame {
     private JButton enquestesAdministradesButton = new JButton("Enquestes Administrades");
     private JButton consultarRecomanacionsButton = new JButton("Consultar Recomanacions");
     /// Faltaria decidir aquesta vista
-    private JButton vetarDesvetarButton = new JButton("Vetar / Desvetar");
+
     private JButton sortirButton = new JButton("Sortir");
 
     /**
@@ -74,7 +74,7 @@ public class VistaAdmin extends JFrame {
         panelBotones.add(enquestesRealitzadesButton);
         panelBotones.add(enquestesAdministradesButton);
         panelBotones.add(consultarRecomanacionsButton);
-        panelBotones.add(vetarDesvetarButton);
+
 
 
         JPanel panelSortir = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -129,9 +129,6 @@ public class VistaAdmin extends JFrame {
         });
         consultarRecomanacionsButton.addActionListener(e -> {
             ctrl.mostrarConsultarRecomanacions();
-        });
-        vetarDesvetarButton.addActionListener(e -> {
-            ctrl.mostrarVetarDesvetar(idUsuari);
         });
 
     }
